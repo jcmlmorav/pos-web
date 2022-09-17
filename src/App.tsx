09af63b3a73dfcd98fc1eb0a71f093932052a5b3
Juan Mora <juan.mora@quantcast.com>
2022-09-17 18:18:17 -0500

    style: setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import Inventory from './views/inventory';
-import { GET_PRODUCTS } from './queries/product';
+import React, { ReactElement } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import Inventory from './views/inventory'
+import { GET_PRODUCTS } from './queries/product'
 
-function App() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
+function App (): ReactElement {
+  const { loading, error, data } = useQuery(GET_PRODUCTS)
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error...</p>;
+  if (loading) return <p>Loading...</p>
+  if (error != null) return <p>Error...</p>
 
   return (
     <BrowserRouter>
@@ -16,7 +16,7 @@ function App() {
         <Route path='/' element={<Inventory data={data.products} />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
